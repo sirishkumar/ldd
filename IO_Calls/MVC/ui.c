48955df3812ea142648a4f7aee08b70819e9a83b
SirishKumar <sirishkumar@gmail.com>
2010-08-10 22:16:45 +0530

    Added books

---
+++ IO_Calls/MVC/ui.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <viml.h>
+
+
+void displaymenu()
+{
+   int choice;
+   char path[100];
+ 
+	while(1){
+
+
+		printf("\nEnter Choice");
+		printf("\n1.Show Image \n2.Convert Image \n3.Save File");
+                scanf("%d",&choice);
+
+                scanf("%s",path);
+               switch(choice){
+
+                 case 1:
+                         printf("\nImage : %s",path);
+                         vdrawImage(path);
+                     break;
+		 case 2:
+                         vconvertImage(path);
+		    break;
+                 case 3:
+                        vsaveImage(path); 
+                     break;
+               }
+
+	}
+    
+}
+
+main()
+{
+
+   displaymenu();
+}
---
+++ IO_Calls/MVC/viml.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include "viml.h"
+
+
+void vdrawImage(char* path)
+{
+
+   printf("\nDrawing Image testi %d",strlen(path));
+
+   char *pImageExt = path[strlen(path)-3];
+
+   printf("%s",pImageExt);
+
+}
+
+
+void vconvertImage(const char*path)
+{
+
+   printf("\nvconertImage");
+
+   char *pImageExt = path[strlen(path)-3];
+
+   printf("%s",pImageExt);
+
+}
+
+void vsaveImage(const char*path)
+{
+
+   printf("\n Image");
+
+   char *pImageExt = path[strlen(path)-3];
+
+   printf("%s",pImageExt);
+
+}
